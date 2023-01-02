629a9c60fe923d8a875c9abef6eb9d4c97e8e638
Pranav Kulkarni <kulkarnipranavmahesh@gmail.com>
2023-01-02 18:58:41 -0500

    chore: home

--- src/app/home/containers/home/home.component.ts
+++ src/app/home/containers/home/home.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewChecked {
+
+  cards;
+  classList: any;
+
+  transition()
+   {
+    console.log('outside');
+
+    if(this.classList) {
+      if (this.classList.contains("active")) {
+        this.classList.remove("active");
+  
+        console.log('removed');
+      } else {
+        console.log('added');
+        this.classList.add("active");
+      }
+    }
+  }
+
+  ngAfterViewChecked(): void {
+    this.cards = document.querySelectorAll(".card");
+
+    console.log(this.cards);
+    this.cards.forEach((card) => card.addEventListener("click", this.transition));
+  }
+  
+  constructor() {
+
+  }
+  
+  
 
 }
